f824850a74bd416b91ced2bd7573b978f2efde1c
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-03-08 01:14:51 +0530

    created signup and signin routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,81 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
+    JWT_SECRET: string,
   }
 }>()
 
-app.post('/api/v1/user/signup', (c) => {
-  
+
+app.post('/api/v1/user/signup', async (c) => {
+  const body = await c.req.json();
+
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
-  return c.text('Hello Hono signup!')
+  }).$extends(withAccelerate())
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name,
+      }
+    })
+
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET)
+    return c.text(jwt)
+  } catch (error) {
+    console.log(error);
+    
+    c.status(411);
+    return c.text("Invalid details")
+  }
 })
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono signin!')
+
+
+
+app.post('/api/v1/user/signin', async (c) => {
+  const body = await c.req.json();
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+        password: body.password,
+      }
+    })
+
+    if(!user) {
+      c.status(403);
+      return c.json({
+        message: "Incorrect creds",
+      })  // unauthorized
+    }
+    
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET)
+
+    return c.text(jwt)
+  } catch (error) {
+    console.log(error);
+    
+    c.status(411);
+    return c.text("Invalid details")
+  }
 })
 app.post('/api/v1/blog', (c) => {
   return c.text('Hello Hono blog!')
