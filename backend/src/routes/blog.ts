27e77e6ff45c385a809eabaa7091009f74789f81
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-03-16 01:45:28 +0530

    Minor backend fixes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { verify } from 'hono/jwt';
+import { createBlogInput, updateBlogInput } from "@lokeshhh/medium-common"
 
 export const blogRouter = new Hono<{
     Bindings: {
@@ -35,8 +36,17 @@ blogRouter.use("/*", async (c, next) => {
 })
 
 blogRouter.post('/', async (c) => {
-    const authorId = c.get("userId");
     const body = await c.req.json();
+
+    const { success } = createBlogInput.safeParse(body);
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct"
+      })
+    }
+
+    const authorId = c.get("userId");
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
@@ -58,6 +68,13 @@ blogRouter.post('/', async (c) => {
 
 blogRouter.put('/', async (c) => {
     const body = await c.req.json();
+    const { success } = updateBlogInput.safeParse(body);
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct"
+      })
+    }
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign } from 'hono/jwt'
+import { signUpInput, signInInput } from '@lokeshhh/medium-common'
 
 
 export const userRouter = new Hono<{
@@ -13,7 +14,13 @@ export const userRouter = new Hono<{
 
 userRouter.post('/signup', async (c) => {
     const body = await c.req.json();
-  
+    const { success } = signUpInput.safeParse(body);
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct"
+      })
+    }
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
@@ -44,6 +51,14 @@ userRouter.post('/signup', async (c) => {
   userRouter.post('/signin', async (c) => {
     const body = await c.req.json();
   
+    const { success } = signInInput.safeParse(body);
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct"
+      })
+    }
+
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
