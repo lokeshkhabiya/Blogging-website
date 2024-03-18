be249a08726abd24726b5352fca947b661c72417
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-03-18 18:12:48 +0530

    Completed blogs/ bulk

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,48 +1,29 @@
 import { Appbar } from "../components/Appbar"
 import { BlogCard } from "../components/BlogCard"
+import { useBlogs } from "../hooks"
 
 const Blogs = () => {
+
+  const {loading, blogs} = useBlogs();
+
+  if (loading) {
+    return <div>
+      Loading...
+    </div>
+  }
+
   return (
     <div>
       <Appbar />
       <div className="flex justify-center">
-        <div className="max-w-xl">
-            <BlogCard 
-              authorName = {"Lokesh"}
-              title = {"How an ugly single page website makes $5000 a month without affiliate marketing"}
-              content = {"How an ugly single page website makes $5000 a month without affiliate marketing How an ugly single page website makes $5000 a month without affiliate marketing"}
+        <div>
+            {blogs.map(blog => <BlogCard 
+              id = {blog.id}
+              authorName = {blog.author.name}
+              title = {blog.title}
+              content = {blog.content}
               publishedDate={"March 18 2024"}
-              />
-            <BlogCard 
-              authorName = {"Lokesh"}
-              title = {"How an ugly single page website makes $5000 a month without affiliate marketing"}
-              content = {"How an ugly single page website makes $5000 a month without affiliate marketing How an ugly single page website makes $5000 a month without affiliate marketing"}
-              publishedDate={"March 18 2024"}
-              />
-            <BlogCard 
-              authorName = {"Lokesh"}
-              title = {"How an ugly single page website makes $5000 a month without affiliate marketing"}
-              content = {"How an ugly single page website makes $5000 a month without affiliate marketing How an ugly single page website makes $5000 a month without affiliate marketing"}
-              publishedDate={"March 18 2024"}
-              />
-            <BlogCard 
-              authorName = {"Lokesh"}
-              title = {"How an ugly single page website makes $5000 a month without affiliate marketing"}
-              content = {"How an ugly single page website makes $5000 a month without affiliate marketing How an ugly single page website makes $5000 a month without affiliate marketing"}
-              publishedDate={"March 18 2024"}
-              />
-            <BlogCard 
-              authorName = {"Lokesh"}
-              title = {"How an ugly single page website makes $5000 a month without affiliate marketing"}
-              content = {"How an ugly single page website makes $5000 a month without affiliate marketing How an ugly single page website makes $5000 a month without affiliate marketing"}
-              publishedDate={"March 18 2024"}
-              />
-            <BlogCard 
-              authorName = {"Lokesh"}
-              title = {"How an ugly single page website makes $5000 a month without affiliate marketing"}
-              content = {"How an ugly single page website makes $5000 a month without affiliate marketing How an ugly single page website makes $5000 a month without affiliate marketing"}
-              publishedDate={"March 18 2024"}
-              />
+            />)}
         </div>
     </div>
   </div>
