baf6e206365e7c96f795f0ec5d16661401c17e64
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-03-24 01:25:51 +0530

    minor changes

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -7,6 +7,9 @@ export const Appbar = () => {
             Bloggs
         </Link>
             <div>
+                <Link to={"/publish"} >
+                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">Publish</button>
+                </Link>
                 <Avatar name = {"Lokesh"} size="big" />
             </div>
     </div>
